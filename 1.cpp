84a0bc8eac922b3cf74bc10ea2ec6292fd69c7f5
unknown <tungle07102005@gmai.l.com>
2024-10-15 14:26:18 +0700

    upd

--- 1.cpp
+++ 1.cpp
@@ -1,12 +1,13 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
+#include <stack>
 using namespace std;
 #define MAX 20
 int a[MAX][MAX];
 int n;
 int c[100];
 int dfs[100];
-int ndfs[100];
+int ndfs=0;
 
 void init() {
 	n = 0;	
@@ -25,6 +26,7 @@ void input() {
 	else {
 		cout << "Khong mo duoc file! \n";
 	}
+	ifs.close();
 }
 
 void output() {
@@ -36,10 +38,52 @@ void output() {
 	}
 }
 
+void outputFile() {
+	ofstream ofs("output.txt");
+	if (ofs.is_open()) {
+		ofs << 1;
+	}
+	else {
+		cout << "Khong mo dc file! \n";
+	}
+}
+
+void khoitaochuaxet() {
+	for(int i = 0; i < n; i++) {
+		c[i] = 1;
+	}
+}
+
+void DFS(int s) //s là đỉnh bắt đầu
+{
+	stack <int> stack;
+	stack.push(s);
+	dfs[ndfs] = s;
+	ndfs++;
+	c[s] = 0;
+	int v = -1, u = s;
+	while (!stack.empty())
+	{
+		if (v == n)
+			stack.pop(u);
+		for (v = 0; v<n; v++)
+			if (a[u][v] != 0 && c[v] == 1)
+			{
+				stack.push(u);
+				stack.push(v);
+				dfs[ndfs] = v;
+				ndfs++;
+				c[v] = 0;
+				u = v;
+				break;
+			}
+	}
+}
+
 int main() {
 	init();
 	input();
-	output();
+	outputFile();
 	system("pause");
 	return 1;
 }
--- b4.cpp
+++ b4.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
-#define MAX 1000
+#define MAX 100000
 
 void input(int a[], int &n) {
 	do {
 		cout << "Nhap so luong phan tu : " << endl;
 		cin >> n;
-	} while (n < 0 || n > 100);
+	} while (n < 0 || n > 100000);
 	for (int i = 0; i < n; i++) {
-		//a[i] = rand() % 100 + 1;
-		cout << "a[" << i << "] = ";
-		cin >> a[i];
+		a[i] = rand() % 100 + 1;
+	/*	cout << "a[" << i << "] = ";
+		cin >> a[i];*/
 	}
 }
 
@@ -37,13 +38,42 @@ void bubbleSort(int a[], int n) {
 	}
 }
 
+void quickSort(int a[], int left, int right) {
+	int pivot = a[(left + right) / 2];
+	int l = left, r = right;
+	do {
+		while (a[l]<pivot) {
+			l++;
+		}
+		while (a[r]>pivot) {
+			r--;
+		}
+		if (l <= r) {
+			swap(a[r], a[l]);
+			l++; r--;
+		}
+	} while (l <= r);
+	if (l<right) {
+		quickSort(a, l, right);
+	}
+	if (left < r) {
+		quickSort(a, left, r);
+	}
+}
+
 int main() {
 	int a[MAX];
 	int n;
+	clock_t s1, e1;
 	input(a, n);
+	s1 = clock();
 	output(a, n);
+	//quickSort(a, 0,n-1);
 	bubbleSort(a, n);
+	e1 = clock();
 	output(a, n);
+	double d1 = (e1 - s1) * 1.0 / CLOCKS_PER_SEC;
+	cout << "Thoi gian chay giai thuat la: " << d1 << endl;
 	system("pause");
 	return 1;
 }
