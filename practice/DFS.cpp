a6d8019e62209005dfa0c91004a4ed81e7b7f815
lekhactung <tungle07102005@gmail.com>
2024-10-19 22:27:16 +0700

    upd

--- practice/DFS.cpp
+++ practice/DFS.cpp
@@ -14,7 +14,7 @@ void dfs(int v,bool visited[],int adj[MAX][MAX],int n){
 }
 
 int main(){
-    ifstream ifs("../data/dfsdata.txt");
+    ifstream ifs("../data/data.txt");
     if(ifs.is_open()){
         int n,e;
         ifs >> n >> e;
@@ -26,18 +26,19 @@ int main(){
             adj[u][v] = adj[v][u] =1;
         }
         dfs(1,visited,adj,n);
-        ofstream ofs("../data/output.txt");
+        ofstream ofs("../data/output.txt",ios::app);
         if(ofs.is_open()){
-            for(int i=1;i<=n;i++){
-                for(int j = 1 ;j <= n ;j++){
-                    ofs << adj[i][j] << " ";
-                }
-                ofs << endl;
-            }    
+            // for(int i=1;i<=n;i++){
+            //     for(int j = 1 ;j <= n ;j++){
+            //         ofs << adj[i][j] << " ";
+            //     }
+            //     ofs << endl;
+            // }    
             ofs <<  "------DFS-------\n";
             for(int i=0;i<ans.size();i++){
                 ofs << ans[i] << " ";
             }
+            ofs << endl;
             ofs.close();
         } else {
             cout << "Khong ghi duoc file! \n";
