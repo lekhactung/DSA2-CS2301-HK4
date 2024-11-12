522990e08e1ea658d0b62bbd774fd0aadf1272f4
lekhactung <tungle07102005@gmail.com>
2024-11-12 19:54:55 +0700

    upd

---
+++ practice/bucketSort.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,m;
+
+
+
+int main(){
+    freopen("../data/minimumSpanningTreeData2.txt","r",stdin);
+    
+
+    return 1;
+}
---
+++ practice/shellSort.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int &a,int &b){
+    int tmp = a; 
+    a = b;
+    b = tmp;
+}
+
+void shellSort(int a[],int n){
+    
+}
+
+
+int main(){
+
+
+
+    return 1;
+}
