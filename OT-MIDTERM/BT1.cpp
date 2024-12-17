9509a50ee49a9c8c9188ff0147b0a65ec7cbbacb
lekhactung <tungle07102005@gmail.com>
2024-12-17 20:35:14 +0700

    upd

--- OT-MIDTERM/BT1.cpp
+++ OT-MIDTERM/BT1.cpp
@@ -222,21 +222,24 @@ void swap(edge &a, edge &b){
 void kruskal(){
     int count = -1;
     for(int i=0;i<vertices_quantity;i++){
-        for (int j=0;j<vertices_quantity;j++){
+        for (int j=i+1;j<vertices_quantity;j++){
             if(matrix[i][j] >0){
                 edge tmp; tmp.u = i; tmp.v = j; tmp.w = matrix[i][j];
                 edges[++count] = tmp;    
             }
         }
     }
-    cout << count << endl;
-    for(int i=0;i<count-1;i++){
-        for(int j = i+1;j<count;j++){
-            if(!cmp(edges[i],edges[j])){
-                swap(edges[i],edges[j]);
-            }
-        }
+    for(int i=0;i<count;i++){
+        cout << edges[i].u << " " << edges[i].v << " " << edges[i].w << endl;
     }
+    // cout << count << endl;
+    // for(int i=0;i<count-1;i++){
+    //     for(int j = i+1;j<count;j++){
+    //         if(!cmp(edges[i],edges[j])){
+    //             swap(edges[i],edges[j]);
+    //         }
+    //     }
+    // }
 }
 
 
